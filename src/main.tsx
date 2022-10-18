ab389658a03c0da10419ad72f7233f0eae62a59c
terao-ryohei <teranyan.tuba@gmail.com>
2022-10-19 02:43:54 +0900

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM, { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const container = document.getElementById('root')
+if (!container) throw new Error('Failed to find the root element')
+
+const root = createRoot(container)
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
